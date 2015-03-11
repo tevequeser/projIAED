e55fd67dc4c6946a68e15130ed340852bbb7f205
gmscs <gserpa@openmailbox.org>
2015-03-11 14:48:05 +0000

    initial commit

---
+++ IAEDproj1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#define inputMAX 60
+
+
+int main()
+{
+	char input[inputMAX];
+	char comando;
+	int i = 0;
+	
+	scanf("%c", input);
+	
+	while(i < strlen(input))
+	{
+		while(input[i] == " " || input[i] == "\t" || input[i] == "\n")
+		{
+			i++;
+		}
+		comando = input[i];
+		
+		
+		
+		
+	
+}
+
+
+
+input = ""
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # projIAED
 Bank stuff 
+
+A Beatriz diz que ela não gosta nada do marco.
